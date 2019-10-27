80f950b5eeaed05516a1121cadd862f84ed0b045
jyp <jyp.123>
2019-10-27 13:41:14 +0800

    测试:提交java项目

---
+++ dubbodemo_consumer/src/main/java/cn/itheima/controller/HelloController.java
@@ -0,0 +1,23 @@
+package cn.itheima.controller;
+
+import cn.itheima.service.HelloService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/demo")
+public class HelloController {
+    @Reference
+    private HelloService helloService;
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String getName(String name){
+        //远程调用
+        String result = helloService.sayHello(name);
+        System.out.println(result);
+        return result;
+    }
+}
